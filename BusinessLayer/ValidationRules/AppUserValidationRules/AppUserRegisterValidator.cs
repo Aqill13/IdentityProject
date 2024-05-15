f02c857962a75fe122614771fd5edec069f052ba
aqil <aqilkalbiyev01@gmail.com>
2024-05-15 15:21:33 +0400

    Add project files.

---
+++ BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,27 @@
+﻿using DTOLayer.DTOs.AppUserDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad boş qoyula bilməz");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad boş qoyula bilməz");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("İstifadəçi adı boş qoyula bilməz");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email boş qoyula bilməz");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifrə boş qoyula bilməz");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Təkrar şifrə boş qoyula bilməz");
+            RuleFor(x => x.Name).MaximumLength(20).WithMessage("Max. 20 hərf girə bilərsiniz");
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage("Min. 3 hərf girə bilərsiniz");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password).WithMessage("Şifrələr uyğun deyil");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Uyğun email girin");
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(T item)
+        {
+            using var context = new Context();
+            context.Set<T>().Remove(item);
+            context.SaveChanges();
+        }
+
+        public T? GetById(int id)
+        {
+            using var context = new Context();
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            using var context = new Context();
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T item)
+        {
+            using var context = new Context();
+            context.Set<T>().Add(item);
+            context.SaveChanges();
+        }
+
+        public void Update(T item)
+        {
+            using var context = new Context();
+            context.Set<T>().Update(item);
+            context.SaveChanges();
+        }
+    }
+}
